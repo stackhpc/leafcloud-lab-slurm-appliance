f3e458b7d7d644cf38be67fe5266405c4f654f17
Steve Brasier <steveb@stackhpc.com>
2025-03-12 14:05:52 +0000

    add production environment

---
+++ environments/production/tofu/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    openstack = {
+      source = "terraform-provider-openstack/openstack"
+    }
+  }
+}
+
+variable "environment_root" {
+    type = string
+    description = "Path to environment root, automatically set by activate script"
+}
+
+
+
+module "cluster" {
+  source = "../../site/tofu/"
+
+  # "hack" for workshops - get cluster name from ~/lab/$LAB_N/..
+  cluster_name = "${split("/", abspath(path.root))[3]}prd"
+
+  compute = {
+    general = {
+      nodes = ["general-0", "general-1"]
+      flavor = "en1.xsmall"
+    }
+  }
+
+  environment_root = var.environment_root
+}
+
